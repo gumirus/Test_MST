6c7513612f457d6e71e79cecaa054f497349d8a1
Ruslan Gumirov <gumirus29@gmail.com>
2025-02-06 21:40:39 +0500

    PX to VW Converter

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('.header');
-  if (window.scrollY > 100) {
-    header.classList.add('header-small');
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('.header')
+  if (window.scrollY > 50) {
+    header.classList.add('header-small')
   } else {
-    header.classList.remove('header-small');
+    header.classList.remove('header-small')
   }
-});
+})
